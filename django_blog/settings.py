43019f93b6ae465548822a93b1261eea6cf66a36
Liga <ligabaikova@gmail.com>
2020-12-05 13:35:23 +0000

    Add environment variables in settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't@+36mn12a(yx@h1hm&ude48_4=9g*iklh-dtx&b_^%6ool$%$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 
 # Application definition
