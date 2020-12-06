25bfa5d4fcd24e020a631d4ccac5fc6d25960e5b
Liga <ligabaikova@gmail.com>
2020-12-06 11:16:40 +0000

    Change databse in settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -82,18 +82,18 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(
-            os.environ.get('DATABASE_URL'))
-    }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'))
+}
 
 
 # Password validation
